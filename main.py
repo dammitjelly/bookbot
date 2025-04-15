bb14d6240ed105b2d6d30e0852b7db2c3b0f9fe8
dammitjelly <dammitjelly@gmail.com>
2025-04-15 14:53:22 +1000

    bookbot proj. complete

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from stats import count_words
 from stats import get_book_text
 from stats import count_characters
 from stats import sort_char_count
+import sys
 
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
-    char_count = count_characters(get_book_text("books/frankenstein.txt"))
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    num_words = count_words(get_book_text(sys.argv[1]))
+    char_count = count_characters(get_book_text(sys.argv[1]))
     sort_chars = sort_char_count(char_count)
 
     print("============ BOOKBOT ============\n"
-        "Analyzing book found at books/frankenstein.txt...")
+       f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------\n"
           f"Found {num_words} total words")
     print("--------- Character Count -------")
